9eb0cc360fe5b67b3ebd1ff05f2c9cf30a8db1d6
Flow <guanjun211@gmail.com>
2024-12-17 11:22:21 +0800

    dd

--- src/js/index.js
+++ src/js/index.js
@@ -1061,16 +1061,16 @@ function generateMobilePage(eventName, eventData) {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta property="og:image:secure_url" content="/public/thumbnails/icon-1024.jpg">
-        <meta name="msapplication-TileImage" content="/public/thumbnails/icon-180.webp">
+        <meta property="og:image:secure_url" content="https://www.theharmony.fun/thumbnails/icon-1024.jpg">
+        <meta name="msapplication-TileImage" content="https://www.theharmony.fun/thumbnails/icon-180.webp">
         <title>The Harmony 禾沐行銷 - ${eventName}</title>
         <link rel="stylesheet" href="styles/style.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@1&display=swap" rel="stylesheet">
-        <link rel="icon" href="/thumbnails/icon-32.webp" sizes="32x32">
-        <link rel="icon" href="/thumbnails/icon-180.webp" sizes="192x192">
-        <link rel="apple-touch-icon" href="/thumbnails/icon-180.webp">
+        <link rel="icon" href="https://www.theharmony.fun//thumbnails/icon-32.webp" sizes="32x32">
+        <link rel="icon" href="https://www.theharmony.fun/thumbnails/icon-180.webp" sizes="192x192">
+        <link rel="apple-touch-icon" href="https://www.theharmony.fun//thumbnails/icon-180.webp">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&display=swap" rel="stylesheet">
